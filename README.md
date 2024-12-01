ebf8bf57599d8468bb5406b415902bf886a6abdb
Minoru Tomobe <minoru.tomobe@gmail.com>
2024-12-01 21:37:14 +0900

    Add untracked files.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# rust-starter
+# SOTAApp2
+Backend web services for the next generation MyACT.
 
 [![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/template/5HAMxu?referralCode=milo)
--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,18 +1,28 @@
-use std::sync::Arc;
+use application::HealthCheck;
+use application::SOTA;
 
-use data_access::{database::ConnectionPool, health::HealthCheckImpl, HealthCheck};
+use data_access::sota::SOTAImpl;
+use data_access::{database::ConnectionPool, health::HealthCheckImpl};
+
+use std::sync::Arc;
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check: Arc<dyn HealthCheck>,
+    sota: Arc<dyn SOTA>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         let health_check = Arc::new(HealthCheckImpl::new(pool.clone()));
-        Self { health_check }
+        let sota = Arc::new(SOTAImpl::new(pool.clone()));
+        Self { health_check, sota }
     }
 
     pub fn health_check(&self) -> Arc<dyn HealthCheck> {
         self.health_check.clone()
+    }
+
+    pub fn sota(&self) -> Arc<dyn SOTA> {
+        self.sota.clone()
     }
 }
