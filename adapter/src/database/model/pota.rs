97185a2098a9b2b5c94c611e57761865f4af868f
Minoru Tomobe <minoru.tomobe@gmail.com>
2024-12-26 00:47:45 +0900

    WIP 12/25

---
+++ adapter/src/database/model/pota.rs
@@ -0,0 +1,59 @@
+use super::common::id::UserId;
+use chrono::NaiveDate;
+
+#[derive(Debug)]
+pub struct ParkCode(String);
+impl ParkCode {
+    pub fn inner_ref(&self) -> &String {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct POTAReference {
+    pub pota_code: String,
+    pub park_name: String,
+    pub park_name_j: String,
+    pub park_location: String,
+    pub park_locid: String,
+    pub park_type: String,
+    pub park_status: bool,
+    pub park_area: i32,
+    pub longitude: Option<f64>,
+    pub lattitude: Option<f64>,
+    pub update: NaiveDate,
+}
+
+#[derive(Debug)]
+pub struct WWFFReference {
+    pub wwff_code: String,
+    pub pota_code: String,
+}
+
+#[derive(Debug)]
+pub struct POTAActivatorLog {
+    pub user_id: UserId,
+    pub dx_entity: String,
+    pub location: String,
+    pub hasc: String,
+    pub pota_code: String,
+    pub park_name: String,
+    pub first_qso_date: NaiveDate,
+    pub attempts: i32,
+    pub activations: i32,
+    pub qsos: i32,
+    pub upload: NaiveDate,
+}
+
+#[derive(Debug)]
+pub struct POTAHunterLog {
+    pub user_id: UserId,
+    pub dx_entity: String,
+    pub location: String,
+    pub hasc: String,
+    pub pota_code: String,
+    pub park_name: String,
+    pub first_qso_date: NaiveDate,
+    pub qsos: i32,
+    pub upload: NaiveDate,
+}
