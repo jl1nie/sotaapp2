b410b56bb346e7139c3ede89bede217294d20ae3
Minoru Tomobe <minoru.tomobe@gmail.com>
2025-01-14 07:14:25 +0900

    Cahnge default alert window.

--- adapter/migrations/sqlite/20241130234000_start.down.sql
+++
@@ -1,9 +0,0 @@
--- Add down migration script here
--- DROP TABLE IF EXISTS sota_references;
---DROP TABLE IF EXISTS pota_references;
---DROP TABLE IF EXISTS wwff_references;
---DROP TABLE IF EXISTS pota_activator_log;
---DROP TABLE IF EXISTS pota_hunter_log;
-DROP TABLE IF EXISTS alerts;
-DROP TABLE IF EXISTS spots;
---DROP TABLE IF EXISTS municipality_century_codes;
